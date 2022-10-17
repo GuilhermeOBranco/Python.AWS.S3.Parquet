ad92a620455309ac4c76e09cc93a4660a20dd525
Guilherme de Oliveira Branco <54077859+GuilhermeOBranco@users.noreply.github.com>
2022-10-17 00:16:06 -0300

    .

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from utils.aws_utils import *
 from datetime import datetime
 from io import BytesIO
 import pandas as pd
-dados = []
 
-file_name = 'usuarios_20221016'
+dados = []
 
 for i in range(650000):
     dados.append({f'Usuario {str(i)}', f'Senha {str(i)}'})
@@ -18,6 +17,6 @@ buffer.seek(0)
 aws_util = aws_utils()
 print(f'inicio: {datetime.now()}')
 
-aws_util.upload(bucket='bucket-boto', key='usuarios_parquet_hoje',
+aws_util.upload(bucket='', key='usuarios_parquet_hoje',
                 file=buffer)
 print(f'fim: {datetime.now()}')
